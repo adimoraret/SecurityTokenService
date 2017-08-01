d5262a9663795a4f47da248aa11cd908b6858c63
Adrian Moraret <adrian.moraret@yahoo.com>
2017-07-31 22:16:39 -0700

    Added initial version for SelfHostWindowsAuthentication

---
+++ SelfHostWindowsAuthentication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostWindowsAuthentication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer3 SelfHost with windows authentication";
+
+            const string url = "https://localhost:44333";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("\n\nServer listening at {0}. Press enter to stop", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SelfHostWindowsAuthentication/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Resources;
+using IdentityServer3.Core.Services.InMemory;
+using Owin;
+using SelfHostWindowsAuthentication.Configuration;
+
+namespace SelfHostWindowsAuthentication
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var options = new IdentityServerOptions
+            {
+                SiteName = "IdentityServer3 - WsFed",
+                SigningCertificate = Certificate.Get(),
+                Factory = CreateInMemoryServiceFactory()
+            };
+
+            appBuilder.UseIdentityServer(options);
+        }
+
+        private static IdentityServerServiceFactory CreateInMemoryServiceFactory()
+        {
+            return new IdentityServerServiceFactory()
+                            .UseInMemoryUsers(new List<InMemoryUser>())
+                            .UseInMemoryClients(Enumerable.Empty<Client>())
+                            .UseInMemoryScopes(Enumerable.Empty<Scope>());
+        }
+    }
+}
